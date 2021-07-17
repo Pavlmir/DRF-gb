f468cbfcb5e148e061b85c6d1d1026cbe6aada19
pavlmir <mirprost@gmail.com>
2021-07-17 23:52:46 +0300

    Урок 1. Урок 1. Введение в REST и Django REST Framework

--- todo_notes/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, UserListView
-
-app_name = "user"
-urlpatterns = [
-    path('user/create/', UserCreateView.as_view()),
-    path('all/', UserListView.as_view()),
-]
