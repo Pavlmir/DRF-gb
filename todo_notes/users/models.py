8caff00c314a0158967df685beef7bbf51474b7a
pavlmir <mirprost@gmail.com>
2021-07-17 23:51:11 +0300

    Урок 1. Урок 1. Введение в REST и Django REST Framework

--- todo_notes/users/models.py
+++ todo_notes/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Users(models.Model):
-    username = models.CharField(verbose_name='Логин', max_length=64)
+class Users(AbstractUser):
+    username = models.CharField(verbose_name='Логин', max_length=64, unique=True)
     firstname = models.CharField(verbose_name='Имя', max_length=64)
     lastname = models.CharField(verbose_name='Фамилия', max_length=64)
     email = models.EmailField(max_length=70, blank=False, unique=True)
-    date_creation = models.PositiveIntegerField(verbose_name='Дата создания записи')
+    date_creation = models.DateTimeField(verbose_name='Дата создания записи', auto_now=True)
     birthday_year = models.DateField(verbose_name='Дата рождения', blank=True, null=True)
     GENDER_TYPE = (
         (1, "Мужской"),
@@ -14,3 +15,6 @@ class Users(models.Model):
         (3, "Не определен"),
     )
     gender = models.IntegerField(verbose_name='Пол', choices=GENDER_TYPE, default=3)
+
+    def __str__(self):
+        return self.username
