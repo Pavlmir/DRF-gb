d7dcdc007cd82729f9846029e18f5e1919eb581b
pavlmir <mirprost@gmail.com>
2021-07-22 20:17:35 +0300

    Урок 2. Введение в React/Vue. Компонентный подход

---
+++ front_vue/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="sticky">
+      <a href="/" class="logo">To-do note</a>
+      <button v-for="tab in tabs" :key="tab.component"  @click="$emit('change-tab', tab)" class="button">
+        {{ tab.title }}
+      </button>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header',
+  props: ['tabs']
+}
+</script>
---
+++ front_vue/src/components/UsersItems.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <td>{{ user.username }}</td>
+    <td>{{ user.firstname }}</td>
+    <td>{{ user.lastname }}</td>
+    <td>{{ user.email }}</td>
+    <td>{{ user.date_creation }}</td>
+    <td>{{ user.birthday_year }}</td>
+    <td>{{ user.gender }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "users-items",
+  props: ["user"],
+};
+</script>
