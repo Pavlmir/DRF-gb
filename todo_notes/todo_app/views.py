d3347d92dca5412e6096684a99e9961fd3b03470
pavlmir <mirprost@gmail.com>
2021-07-29 19:24:16 +0300

    Урок 4. Урок 4. Views. Filtering. Pagination

--- todo_notes/todo_app/views.py
+++ todo_notes/todo_app/views.py
@@ -1,15 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
 # В классе ModelViewSet уже есть методы create, update, list, retrieve, delete методами
 class ProjectViewSet(viewsets.ModelViewSet):
-    serializer_class = ProjectModelSerializer
     queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
 
 
 # В классе ModelViewSet уже есть методы create, update, list, retrieve, delete методами
 class ToDoViewSet(viewsets.ModelViewSet):
-    serializer_class = ToDoModelSerializer
     queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
